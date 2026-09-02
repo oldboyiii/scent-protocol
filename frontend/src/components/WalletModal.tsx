8d4c8a4f82d50ab5da815c490aeada7e8135dfdf
oldboyiii <41637112+oldboyiii@users.noreply.github.com>
2026-09-02 12:16:44 +0300

    Update WalletModal.tsx

--- frontend/src/components/WalletModal.tsx
+++ frontend/src/components/WalletModal.tsx
@@ -11,51 +11,68 @@ interface WalletOption {
   check: () => boolean;
 }
 
-// Official wallet icons from trusted CDN sources
+// Official wallet icons as inline SVG
 const WalletIcons = {
-  // MetaMask - using img tag with official CDN
+  // MetaMask
   metamask: (
-    <img 
-      src="https://raw.githubusercontent.com/MetaMask/brand-resources/master/SVG/metamask-fox.svg" 
-      alt="MetaMask"
-      className="w-8 h-8"
-    />
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M29.5 4L17 10.5L18.5 7L29.5 4Z" fill="#E2761B"/>
+      <path d="M2.5 4L14.8 10.8L13.5 7L2.5 4Z" fill="#E4761B"/>
+      <path d="M25.5 23L23 27.5L28.5 29L30 23.5L25.5 23Z" fill="#E4761B"/>
+      <path d="M4 23.5L5.5 29L11 27.5L8.5 23L4 23.5Z" fill="#E4761B"/>
+      <path d="M10.5 20L9 23.5L14.5 23.5L14 18.5L10.5 20Z" fill="#D7C1B3"/>
+      <path d="M21.5 20L18 18.5L17.5 23.5L23 23.5L21.5 20Z" fill="#D7C1B3"/>
+      <path d="M11 27.5L14.5 25.5L12.5 24L11 27.5Z" fill="#233447"/>
+      <path d="M21 27.5L19.5 24L17.5 25.5L21 27.5Z" fill="#233447"/>
+      <path d="M23 27.5L21 23.5L19.5 25L19.5 27.5L23 27.5Z" fill="#CD6116"/>
+      <path d="M9 27.5L12.5 27.5L12.5 25L11 23.5L9 27.5Z" fill="#CD6116"/>
+      <path d="M15 19.5L14 22L17.5 20.5L18 19.5L15 19.5Z" fill="#E4751F"/>
+      <path d="M17 19.5L17.5 20.5L21 22L20.5 19.5L17 19.5Z" fill="#E4751F"/>
+      <path d="M20.5 22L17.5 20.5L18.5 23L18 25.5L20.5 22Z" fill="#F6851B"/>
+      <path d="M11.5 22L14 25.5L13.5 23L14.5 20.5L11.5 22Z" fill="#F6851B"/>
+      <path d="M17.5 25.5L18.5 23L13.5 23L14.5 25.5L17.5 25.5Z" fill="#C0AD9E"/>
+      <path d="M21 25.5L18.5 23L17.5 25.5L21 25.5Z" fill="#C0AD9E"/>
+      <path d="M21 25.5L22.5 27.5L24 25.5L21 25.5Z" fill="#161616"/>
+      <path d="M11 25.5L12.5 27.5L14 25.5L11 25.5Z" fill="#161616"/>
+    </svg>
   ),
-  
-  // Rabby - from official website
+
+  // Rabby
   rabby: (
-    <img 
-      src="https://rabby.io/assets/logo-256.png" 
-      alt="Rabby"
-      className="w-8 h-8 rounded-full"
-    />
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="32" height="32" rx="6" fill="#8285EB"/>
+      <path d="M16 8C11.582 8 8 11.582 8 16C8 20.418 11.582 24 16 24C20.418 24 24 20.418 24 16C24 11.582 20.418 8 16 8ZM16 21C13.239 21 11 18.761 11 16C11 13.239 13.239 11 16 11C18.761 11 21 13.239 21 16C21 18.761 18.761 21 16 21Z" fill="white"/>
+      <circle cx="16" cy="16" r="2.5" fill="white"/>
+    </svg>
   ),
-  
-  // Coinbase Wallet - official icon
+
+  // Coinbase
   coinbase: (
-    <img 
-      src="https://www.coinbase.com/favicon.ico" 
-      alt="Coinbase"
-      className="w-8 h-8"
-    />
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="16" cy="16" r="16" fill="#0052FF"/>
+      <path d="M16 8C11.582 8 8 11.582 8 16C8 20.418 11.582 24 16 24C20.418 24 24 20.418 24 16C24 11.582 20.418 8 16 8ZM19 17H13V15H19V17Z" fill="white"/>
+    </svg>
   ),
-  
-  // Trust Wallet - official icon
+
+  // Trust Wallet
   trust: (
-    <img 
-      src="https://trustwallet.com/assets/images/favicon.png" 
-      alt="Trust Wallet"
-      className="w-8 h-8"
-    />
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="16" cy="16" r="16" fill="#3375BB"/>
+      <path d="M16 3L4 9V16C4 22.5 8.5 27.5 16 29C23.5 27.5 28 22.5 28 16V9L16 3Z" fill="white" fillOpacity="0.2"/>
+      <path d="M16 6L7 10.5V16C7 20.5 10.5 25 16 26.5C21.5 25 25 20.5 25 16V10.5L16 6Z" fill="white" fillOpacity="0.3"/>
+      <path d="M16 9L10 12.5V16C10 19 12.5 22 16 23C19.5 22 22 19 22 16V12.5L16 9Z" fill="white"/>
+    </svg>
   ),
-  
-  // OKX Wallet - official icon
+
+  // OKX
   okx: (
-    <img 
-      src="https://www.okx.com/favicon.ico" 
-      alt="OKX"
-      className="w-8 h-8"
-    />
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="32" height="32" rx="6" fill="#000000"/>
+      <rect x="10" y="10" width="4" height="4" fill="#FFFFFF"/>
+      <rect x="18" y="10" width="4" height="4" fill="#FFFFFF"/>
+      <rect x="10" y="18" width="4" height="4" fill="#FFFFFF"/>
+      <rect x="18" y="18" width="4" height="4" fill="#FFFFFF"/>
+    </svg>
   ),
 };
 
