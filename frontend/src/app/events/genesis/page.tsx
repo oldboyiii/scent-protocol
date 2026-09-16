3534cd735ccfd75eafdf2d5934819c43a477b212
oldboyiii <41637112+oldboyiii@users.noreply.github.com>
2026-09-16 20:01:40 +0300

    Update page.tsx

--- frontend/src/app/events/genesis/page.tsx
+++ frontend/src/app/events/genesis/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { ethers } from "ethers";
 import { useWallet } from "@/context/WalletContext";
 
-const GENESIS_CONTRACT_ADDRESS = "0xcBc9c225495B1086EA0eA3574ceB473C1f4b35c9";
+const GENESIS_CONTRACT_ADDRESS = "0x1152E29703313B49BAD9560af64458E24C785E2B";
 
 const GENESIS_ABI = [
   "function requestMint() external returns (uint256)",
